bc1c9f8e5b42ff574e674e8fe12a57c197d82668
Roque Arnas <roque.arnas@didesweb.com>
2015-03-04 20:00:03 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE HTML>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<title>Didesweb, proteger el campo email</title>
+	<!-- Didesweb -->  
+	<!-- Diseño y desarrollo web -->  
+	<!-- http://didesweb.com/-->  
+	<!-- Este obra está protegida bajo licencia Creative Commons Attribution --> 
+</head>
+<body>
+	<h1>&copy; Didesweb, proteger el campo email</h1>
+	<form action="funcion_validar_mail.php" method="POST"> 
+	<input type="text" name="variable_input"/>
+	<input type="submit" value="enviar"/>
+	</form>
+</body>
+</html>
